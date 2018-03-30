1eea3f4d82a13ab5603a1e3d9e7950b2e393e356
TriEightz <yujin_g1995@hotmail.com>
2018-03-30 15:38:03 +0800

    Added test for profilesAddTag and profiles DeleteTag

--- imports/api/profiles.test.js
+++ imports/api/profiles.test.js
@@ -6,21 +6,60 @@ import { ProfilesDB } from "./profiles";
 if (Meteor.isServer) {
     describe("profiles", function() {
         describe("profileJoinGroup", function() {
-        const userId = "testId";
-        beforeEach(function() {
-            ProfilesDB.remove({});
-            ProfilesDB.insert({ _id: userId, groups: [] });
+            const userId = "testId";
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [] });
+            });
+
+            it("should allow a user to join a group", function() {
+                Meteor.server.method_handlers.profilesJoinGroup.apply({ userId }, [
+                    "group1"
+                ]);
+
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).groups.includes("group1")
+                ).toBeTruthy();
+            });
         });
 
-        it("should allow a user to join a group", function() {
-            Meteor.server.method_handlers.profilesJoinGroup.apply({ userId }, [
-                "group1"
-            ]);
+        describe("profilesAddTag", function() {
+            const userId = "testId";
+            const tag = "#food"
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [] });
+            });
+    
+            it("should add tags to the user profile", function() {
+                Meteor.server.method_handlers.profilesAddTag.apply({ userId }, [
+                    tag
+                ]);
 
-            expect(
-                ProfilesDB.findOne({ _id: userId }).groups.includes("group1")
-            ).toBeTruthy();
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+                ).toBeTruthy();
+            });
         });
-    });
+
+        describe("profilesRemoveTag", function() {
+            const userId = "testId";
+            const tag = "#food"
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [] });
+            });
+    
+            it("should add tags to the user profile", function() {
+                Meteor.server.method_handlers.profilesAddTag.apply({ userId }, [
+                    tag
+                ]);
+
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+                ).toBeTruthy();
+            });
+        });
+        
     });
 }
