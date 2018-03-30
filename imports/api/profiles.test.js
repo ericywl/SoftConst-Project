ef6803d0ab292d9b33356852f6cbd7ddca1cbecb
TriEightz <yujin_g1995@hotmail.com>
2018-03-30 18:15:40 +0800

    tests done for profile functions

--- imports/api/profiles.test.js
+++ imports/api/profiles.test.js
@@ -42,24 +42,75 @@ if (Meteor.isServer) {
             });
         });
 
-        describe("profilesRemoveTag", function() {
+        // describe("profilesRemoveTag", function() {
+        //     const userId = "testId";
+        //     const tag = "#food"
+        //     beforeEach(function() {
+        //         ProfilesDB.remove({});
+        //         ProfilesDB.insert({ _id: userId, groups: [], tags: ["#food"] });
+        //     });
+    
+        //     it("should remove the tag from the user profile", function() {
+        //         Meteor.server.method_handlers.profilesRemoveTag.apply({userId}, 
+        //             [userId, tag]
+        //         );
+        //         expect(
+        //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+        //         ).toBeFalsy();
+        //     });
+        // });
+        
+        // describe("profilesUpdateDisplayName", function() {
+        //     const userId = "testId";
+        //     beforeEach(function() {
+        //         ProfilesDB.remove({});
+        //         ProfilesDB.insert({ _id: userId, groups: [] });
+        //     });
+    
+        //     it("should update user profile's diaplay name", function() {
+        //         Meteor.server.method_handlers.profilesUpdateDisplayName.apply({ userId }, [
+        //         ]);
+
+        //         expect(
+        //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+        //         ).toBeTruthy();
+        //     });
+        // });
+
+        describe("profilesUpdateBio", function() {
             const userId = "testId";
-            const tag = "#food"
             beforeEach(function() {
                 ProfilesDB.remove({});
                 ProfilesDB.insert({ _id: userId, groups: [] });
             });
     
-            it("should add tags to the user profile", function() {
-                Meteor.server.method_handlers.profilesAddTag.apply({ userId }, [
-                    tag
+            it("should update the user profile's biography", function() {
+                Meteor.server.method_handlers.profilesUpdateBio.apply({ userId }, [
+                    "User's biography"
                 ]);
 
                 expect(
-                    ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+                    ProfilesDB.findOne({ _id: userId }).bio.includes("User's biography")
                 ).toBeTruthy();
             });
         });
-        
+
+        describe("profilesInsert", function() {
+            const userId = "testId";
+            beforeEach(function() {
+                ProfilesDB.remove({});
+            });
+    
+            it("should insert a new profile", function() {
+                Meteor.server.method_handlers.profilesInsert.apply({}, [
+                    userId, "User Display Name"
+                ]);
+
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).displayName.includes("User Display Name")
+                ).toBeTruthy();
+            });
+        });
+
     });
 }
