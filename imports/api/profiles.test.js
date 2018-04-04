8d7748c4a728ffb306d3cabe0f0088746083ae72
TriEightz <yujin_g1995@hotmail.com>
2018-04-04 11:59:47 +0800

    updated tests

--- imports/api/profiles.test.js
+++ imports/api/profiles.test.js
@@ -1,118 +1,118 @@
-// import expect from "expect";
+import expect from "expect";
 
-// import { GroupsDB } from "./groups";
-// import { ProfilesDB } from "./profiles";
+import { GroupsDB } from "./groups";
+import { ProfilesDB } from "./profiles";
 
-// if (Meteor.isServer) {
-//     describe("profiles", function() {
-//         describe("profileJoinGroup", function() {
-//             const userId = "testId";
-//             beforeEach(function() {
-//                 ProfilesDB.remove({});
-//                 ProfilesDB.insert({ _id: userId, groups: [] });
-//                 GroupsDB.remove({});
-//                 GroupsDB.insert({ _id: "groupId1", lastMessageAt: 0 });
-//             });
+if (Meteor.isServer) {
+    describe("profiles", function() {
+        describe("profileJoinGroup", function() {
+            const userId = "testId";
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [], ownedBy: "notyou" });
+                GroupsDB.remove({});
+                GroupsDB.insert({ _id: "groupId1", members:[] });
+            });
 
-//             it("should allow a user to join a group", function() {
-//                 Meteor.server.method_handlers.profilesJoinGroup.apply({ userId }, [
-//                     "groupId1"
-//                 ]);
+            it("should allow a user to join a group", function() {
+                Meteor.server.method_handlers.profilesJoinGroup.apply({ userId }, [
+                    "groupId1"
+                ]);
 
-//                 expect(
-//                     ProfilesDB.findOne({ _id: userId }).groups.includes("groupId1")
-//                 ).toBeTruthy();
-//             });
-//         });
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).groups.includes("groupId1")
+                ).toBeTruthy();
+            });
+        });
 
-//         describe("profilesAddTag", function() {
-//             const userId = "testId";
-//             const tag = "#food"
-//             beforeEach(function() {
-//                 ProfilesDB.remove({});
-//                 ProfilesDB.insert({ _id: userId, groups: [] });
-//             });
+        describe("profilesAddTag", function() {
+            const userId = "testId";
+            const tag = "#food"
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [] });
+            });
     
-//             it("should add tags to the user profile", function() {
-//                 Meteor.server.method_handlers.profilesAddTag.apply({ userId }, [
-//                     tag
-//                 ]);
+            it("should add tags to the user profile", function() {
+                Meteor.server.method_handlers.profilesAddTag.apply({ userId }, [
+                    tag
+                ]);
 
-//                 expect(
-//                     ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
-//                 ).toBeTruthy();
-//             });
-//         });
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+                ).toBeTruthy();
+            });
+        });
 
-//         // describe("profilesRemoveTag", function() {
-//         //     const userId = "testId";
-//         //     const tag = "#food"
-//         //     beforeEach(function() {
-//         //         ProfilesDB.remove({});
-//         //         ProfilesDB.insert({ _id: userId, groups: [], tags: ["#food"] });
-//         //     });
+        // describe("profilesRemoveTag", function() {
+        //     const userId = "testId";
+        //     const tag = "#food"
+        //     beforeEach(function() {
+        //         ProfilesDB.remove({});
+        //         ProfilesDB.insert({ _id: userId, groups: [], tags: ["#food"] });
+        //     });
     
-//         //     it("should remove the tag from the user profile", function() {
-//         //         Meteor.server.method_handlers.profilesRemoveTag.apply({userId}, 
-//         //             [userId, tag]
-//         //         );
-//         //         expect(
-//         //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
-//         //         ).toBeFalsy();
-//         //     });
-//         // });
+        //     it("should remove the tag from the user profile", function() {
+        //         Meteor.server.method_handlers.profilesRemoveTag.apply({userId}, 
+        //             [userId, tag]
+        //         );
+        //         expect(
+        //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+        //         ).toBeFalsy();
+        //     });
+        // });
         
-//         // describe("profilesUpdateDisplayName", function() {
-//         //     const userId = "testId";
-//         //     beforeEach(function() {
-//         //         ProfilesDB.remove({});
-//         //         ProfilesDB.insert({ _id: userId, groups: [] });
-//         //     });
+        // describe("profilesUpdateDisplayName", function() {
+        //     const userId = "testId";
+        //     beforeEach(function() {
+        //         ProfilesDB.remove({});
+        //         ProfilesDB.insert({ _id: userId, groups: [] });
+        //     });
     
-//         //     it("should update user profile's diaplay name", function() {
-//         //         Meteor.server.method_handlers.profilesUpdateDisplayName.apply({ userId }, [
-//         //         ]);
+        //     it("should update user profile's diaplay name", function() {
+        //         Meteor.server.method_handlers.profilesUpdateDisplayName.apply({ userId }, [
+        //         ]);
 
-//         //         expect(
-//         //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
-//         //         ).toBeTruthy();
-//         //     });
-//         // });
+        //         expect(
+        //             ProfilesDB.findOne({ _id: userId }).tags.includes("#food")
+        //         ).toBeTruthy();
+        //     });
+        // });
 
-//         describe("profilesUpdateBio", function() {
-//             const userId = "testId";
-//             beforeEach(function() {
-//                 ProfilesDB.remove({});
-//                 ProfilesDB.insert({ _id: userId, groups: [] });
-//             });
+        describe("profilesUpdateBio", function() {
+            const userId = "testId";
+            beforeEach(function() {
+                ProfilesDB.remove({});
+                ProfilesDB.insert({ _id: userId, groups: [] });
+            });
     
-//             it("should update the user profile's biography", function() {
-//                 Meteor.server.method_handlers.profilesUpdateBio.apply({ userId }, [
-//                     "User's biography"
-//                 ]);
+            it("should update the user profile's biography", function() {
+                Meteor.server.method_handlers.profilesUpdateBio.apply({ userId }, [
+                    "User's biography"
+                ]);
 
-//                 expect(
-//                     ProfilesDB.findOne({ _id: userId }).bio.includes("User's biography")
-//                 ).toBeTruthy();
-//             });
-//         });
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).bio.includes("User's biography")
+                ).toBeTruthy();
+            });
+        });
 
-//         describe("profilesInsert", function() {
-//             const userId = "testId";
-//             beforeEach(function() {
-//                 ProfilesDB.remove({});
-//             });
+        describe("profilesInsert", function() {
+            const userId = "testId";
+            beforeEach(function() {
+                ProfilesDB.remove({});
+            });
     
-//             it("should insert a new profile", function() {
-//                 Meteor.server.method_handlers.profilesInsert.apply({}, [
-//                     userId, "User Display Name"
-//                 ]);
+            it("should insert a new profile", function() {
+                Meteor.server.method_handlers.profilesInsert.apply({}, [
+                    userId, "User Display Name"
+                ]);
 
-//                 expect(
-//                     ProfilesDB.findOne({ _id: userId }).displayName.includes("User Display Name")
-//                 ).toBeTruthy();
-//             });
-//         });
+                expect(
+                    ProfilesDB.findOne({ _id: userId }).displayName.includes("User Display Name")
+                ).toBeTruthy();
+            });
+        });
 
-//     });
-// }
+    });
+}
