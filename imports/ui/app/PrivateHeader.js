8b8339d274222fee95eb00e46b656f41dff730d6
ericywl <midnight2903@gmail.com>
2018-04-20 09:50:21 +0800

    moment

--- imports/ui/app/PrivateHeader.js
+++ imports/ui/app/PrivateHeader.js
@@ -2,6 +2,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withTracker } from "meteor/react-meteor-data";
+import moment from "moment";
 
 // APIs
 import history from "../../startup/history";
